dedad46eefefda571b9369e812715936f429ed69
Jaimini <jaiminiraval123@gmail.com>
2023-10-23 14:47:24 +0530

    Vaults model add

---
+++ server/model/Password.js
@@ -0,0 +1,28 @@
+const mongoose =require('mongoose');
+const passwordSchema = mongoose.Schema({
+    passwordName: {
+        type: String,
+        default:null,
+        required:true
+    },
+    username: {
+        type: String,
+        default:null,
+        required:true
+    },
+    password: {
+        type: String,
+        default:null,
+        required:true
+    },
+    //to which folder category will that password belong refering to folder model
+
+    folderid:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Folder',
+        required: true
+
+    }
+})
+const Password = mongoose.model('Password',passwordSchema)
+module.exports = Password
