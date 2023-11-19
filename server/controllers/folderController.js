32b42962244294825b5e2bc180f4dde5fe6ea552
Jaimini <jaiminiraval123@gmail.com>
2023-11-19 11:44:56 +0530

    Delete folder

--- server/controllers/folderController.js
+++ server/controllers/folderController.js
@@ -1,15 +1,64 @@
 const Folder = require('../model/Folder');
-const getAll = ( async (req,res)=>{
-    const folder = await Folder.find();
-    res.send(folder);
+const getAll = (async (req, res) => {
+    try {
+        const folder = await Folder.find();
+        res.send(folder);
+    }
+    catch (error) {
+        res.status(400).send('Cannot get folders!');
+    }
+
 })
-const addFolder = ( async (req,res)=>{
-    const folderName = req.body.folderName;
-    const folder = new Folder({
-        folderName
-    })
-    await folder.save();
-    res.json(folder)
-    res.send(folder);
+const addFolder = (async (req, res) => {
+    try {
+        const folderName = req.body.folderName;
+        const folder = new Folder({
+            folderName
+        })
+        await folder.save();
+        res.json(folder);
+        res.send(folder);
+    } catch (error) {
+        res.status(400).json({ error: "Folder exists!" })
+    }
+
 })
-module.exports = {addFolder,getAll};
+// const deleteFolder = (async (req,res)=>{
+//     try {
+//         //const folderid = req.params.folderid;
+//         const folder = await Folder.findById({_id:req.params.folderid});
+//         if(!folder){
+//             res.status(400).json({error:'Folder not found'});
+//         }
+//         await Folder.findByIdAndDelete({_id:req.params.folderid});
+
+//     } catch (error) {
+//         res.status(500).json({error : error.message});
+//     }
+// })
+const deleteFolder = async (req, res) => {
+    try {
+        const folderid = req.params.folderid;
+        console.log("Folder ID:", folderid);
+
+        // Check if the folder exists
+        const folder = await Folder.findById(folderid);
+        console.log("Found Folder:", folder);
+
+        if (!folder) {
+            console.log("Folder not found");
+            return res.status(404).json({ error: 'Folder not found' });
+        }
+
+        // Delete the folder
+        await Folder.findByIdAndDelete(folderid);
+
+        console.log("Folder deleted successfully");
+        res.json({ message: 'Folder deleted successfully', deletedFolder: folder });
+    } catch (error) {
+        console.error('Error deleting folder:', error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+};
+
+module.exports = { addFolder, getAll,deleteFolder };
--- server/routers/folderRoutes.js
+++ server/routers/folderRoutes.js
@@ -3,4 +3,5 @@ const {Router } = require('express');
 const router = Router();
 router.post("/addfolder",folderController.addFolder);
 router.get("/",folderController.getAll);
+router.delete("/:folderid",folderController.deleteFolder);
 module.exports = router;
