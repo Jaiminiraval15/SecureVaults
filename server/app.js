8e32c50cad52ddf63f021b8bef18342aebb30442
Jaimini <jaiminiraval123@gmail.com>
2023-11-25 12:24:47 +0530

    add auth middleware

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ const PORT = process.env.port || 3000;
 const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
+const requireAuth = require('./middleware/authMiddleware')
 mongoose.connect(process.env.db_url,{
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -16,6 +17,6 @@ app.use(express.json());
 const folderRoutes = require('./routers/folderRoutes')
 const passwordRoutes = require('./routers/passwordRoutes')
 const authRoutes = require('./routers/authRoutes')
-app.use('/api/folder',folderRoutes);
+app.use('/api/folder',requireAuth,folderRoutes);
 app.use('/api/password',passwordRoutes);
 app.use('/api/routes',authRoutes);
