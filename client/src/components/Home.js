5f62d13facdee905c497245a1307b864d1bedd50
Jaimini <jaiminiraval123@gmail.com>
2023-12-17 14:42:09 +0530

    Add login,signup buttons

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,30 +1,24 @@
-
-import { purple } from "@mui/material/colors";
-import Nav from "./Navbar";
 import { Container, Grid, Typography } from '@mui/material'
 
 export default function Home() {
     return (
         <>
-
-           
             <Container maxWidth='lg'>
                 <Grid container columnSpacing={4}>
                     <Grid item xs={12} md={6}>
-
                         <Typography variant="h5" mt={15} style={{ fontWeight: 'bolder', color: 'purple' }}>
                             Privacy's Finest Hour: One Password to Rule Them All
-                            </Typography>
-                            <Typography variant="body1" style={{ color: 'purple', fontFamily: 'revert' }}>
-              Your data remains confidential and is inaccessible to anyone,<br />
-              including us<br />
-              Your master password is the exclusive key to unveil and manage your confidential vaults<br />
-            </Typography>
+                        </Typography>
+                        <Typography variant="body1" style={{ color: 'purple', fontFamily: 'revert' }}>
+                            Your data remains confidential and is inaccessible to anyone,<br />
+                            including us<br />
+                            Your master password is the exclusive key to unveil and manage your confidential vaults<br />
+                        </Typography>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                    <img src='/static/passw.jpeg'style={{ maxWidth: '100%', height: 'auto' }}  ></img>
+                        <img src='/static/passw.jpeg' style={{ maxWidth: '100%', height: 'auto' }}  ></img>
+                    </Grid>
                 </Grid>
-            </Grid>
             </Container>
         </>
     )
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,17 +1,32 @@
 import {Navbar} from 'react-bootstrap';
-
+import {Button} from '@mui/material';
 export default function Nav() {
     return(
         <>
-        <Navbar expand="lg" className="bg-body-tertiary" style={{
+        <Navbar expand="lg" className="bg-body-tertiary d-flex align-items-center" style={{
             color : 'purple',
             padding: '0.5em',
-            display : 'block',
+            display : 'inline',
             fontSize : '2em',
             float : 'left',
             fontFamily : 'fantasy' ,
             position : 'static' 
         }}>SecureVaults</Navbar>
+        <Button variant="outlined" style={{ color : 'purple',
+            padding: '0.5em',
+            display : 'block',
+            float:"right",
+            marginRight:'1em',
+            marginTop:'0.5em'
+            }} >Signup</Button>
+        <Button variant="outlined" color="primary" style={{ color : 'purple',
+            padding: '0.5em',
+            display : 'block',
+            float:"right",
+            marginRight:'1em',
+            marginTop:'0.5em'
+            }} >Login</Button>
+            
         </>
     )
 }
