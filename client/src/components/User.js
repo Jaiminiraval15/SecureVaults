e2da23488549d6fb8fa3954920133b87504fccf5
Jaimini <jaiminiraval123@gmail.com>
2023-12-25 13:07:53 +0530

    Conditional render of buttons

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,58 +1,81 @@
 
-import { Button} from '@mui/material';
+import { Button } from '@mui/material';
 import LoginForm from './Forms/LoginForm';
 import SignupForm from './Forms/SignupForm';
 import { useState } from 'react';
 import { useLogout } from '../hooks/useLogout';
-export default function User(){
-    const [login,setLogin] = useState(false);
-    const [signup,setSignup] = useState(false);
-    const openLogin = ()=>{
+import { useAuthContext } from '../hooks/useAuthContext';
+
+export default function User() {
+    const [login, setLogin] = useState(false);
+    const [signup, setSignup] = useState(false);
+    const openLogin = () => {
         setLogin(true)
     }
-    const openSignup = ()=>{
+    const openSignup = () => {
         setSignup(true)
     }
-    const {logout} = useLogout()
-    const handleSubmit = () =>{
+    const { logout } = useLogout()
+    const handleSubmit = () => {
         logout()
     }
-    return(
+    const { user } = useAuthContext()
+    return (
         <>
-         <Button variant="outlined" color="primary" style={{
+            {user &&
+                ( 
+                    <Button variant="outlined" color="primary" style={{
+                    color: 'purple',
+                    padding: '0.5em',
+                    display: 'block',
+                    float: "right",
+                    marginRight: '1em',
+                    marginTop: '0.5em'
+                }}
+                    onClick={handleSubmit}
+
+                >Logout</Button>
+         
+                )   
+            }
+      {!user && (
+  <>
+    <Button
+      variant="outlined"
+      style={{
         color: 'purple',
-                padding: '0.5em',
-                display: 'block',
-                float: "right",
-                marginRight: '1em', 
-                marginTop: '0.5em'
-    }}
-    onClick={handleSubmit}
-   
-    >Logout</Button>
-       
-            <Button variant="outlined"  style={{
-                color: 'purple',
-                padding: '0.5em',
-                display: 'block',
-                float: "right",
-                marginRight: '1em',
-                marginTop: '0.5em'
-    }} onClick={openSignup}
-    >Signup</Button>
-     <Button variant="outlined" color="primary" style={{
+        padding: '0.5em',
+        display: 'block',
+        float: 'right',
+        marginRight: '1em',
+        marginTop: '0.5em'
+      }}
+      onClick={openSignup}
+    >
+      Signup
+    </Button>
+    <Button
+      variant="outlined"
+      color="primary"
+      style={{
         color: 'purple',
-                padding: '0.5em',
-                display: 'block',
-                float: "right",
-                marginRight: '1em',
-                marginTop: '0.5em'
-    }}
-    onClick={openLogin}
-   
-    >Login</Button>
-          {signup && <SignupForm />}
-      {login && <LoginForm />}
+        padding: '0.5em',
+        display: 'block',
+        float: 'right',
+        marginRight: '1em',
+        marginTop: '0.5em'
+      }}
+      onClick={openLogin}
+    >
+      Login
+    </Button>
+    {signup && <SignupForm />}
+    {login && <LoginForm />}
+  </>
+)}
+
+ 
+
         </>
     )
 }
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -1,9 +1,16 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer,useEffect } from "react";
 import { authReducer, initialState } from "../reducers/authReducer";
 export const AuthContext = createContext();
 
 export const AuthContextProvider = ({children}) =>{
     const [state,dispatch] = useReducer(authReducer,initialState)
+   
+    useEffect(()=>{
+        const user = JSON.parse(localStorage.getItem('user'))
+        if(user){
+            dispatch({type: 'LOGIN',payload:user})
+        }
+    },[])
     return(
      <AuthContext.Provider value={{...state,dispatch}}>
         {children}
