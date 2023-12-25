98515acbe029c46ef3b6b31d2bbb0dbd951cd2e4
Jaimini <jaiminiraval123@gmail.com>
2023-12-25 11:22:30 +0530

    Add useLogout hook

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -3,6 +3,7 @@ import { Button} from '@mui/material';
 import LoginForm from './Forms/LoginForm';
 import SignupForm from './Forms/SignupForm';
 import { useState } from 'react';
+import { useLogout } from '../hooks/useLogout';
 export default function User(){
     const [login,setLogin] = useState(false);
     const [signup,setSignup] = useState(false);
@@ -12,8 +13,23 @@ export default function User(){
     const openSignup = ()=>{
         setSignup(true)
     }
+    const {logout} = useLogout()
+    const handleSubmit = () =>{
+        logout()
+    }
     return(
         <>
+         <Button variant="outlined" color="primary" style={{
+        color: 'purple',
+                padding: '0.5em',
+                display: 'block',
+                float: "right",
+                marginRight: '1em', 
+                marginTop: '0.5em'
+    }}
+    onClick={handleSubmit}
+   
+    >Logout</Button>
        
             <Button variant="outlined"  style={{
                 color: 'purple',
