ec1fecccf5c77027ae1fd6cb47a4943b3e305809
Jaimini <jaiminiraval123@gmail.com>
2024-01-01 10:35:40 +0530

    Fix encryptionKey logic

--- client/src/reducers/keyReducer.js
+++ client/src/reducers/keyReducer.js
@@ -4,10 +4,10 @@ export const initialState = {
 export const keyReducer = (state,action) => {
     switch(action.type){
         case 'SET_KEY' :
-           window.sessionStorage.setItem("key",action.payload)
+           window.localStorage.setItem("key",action.payload)
            return action.payload
         case 'CLEAR_KEY' :
-            window.sessionStorage.removeItem("key")
+            window.localStorage.removeItem("key")
             return ""
         default :
             return state
