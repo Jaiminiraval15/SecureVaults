4162f7d7421ec1a3194662b6115009de24670016
Jaimini <jaiminiraval123@gmail.com>
2024-01-08 12:28:59 +0530

    remove unnecessary code

--- client/src/App.js
+++ client/src/App.js
@@ -7,40 +7,12 @@ import {BrowserRouter,Routes,Route, Navigate ,Outlet} from 'react-router-dom'
 
 import { EncryptionContext, EncryptionContextProvider } from './context/EncryptionContext';
 import Folder from './components/Folder/folder';
-
 import { useAuthContext } from './hooks/useAuthContext';
-function PrivateRoute({ children,...rest }) {
-  const { user } = useAuthContext();
-  return (
-    
-    <Route {...rest}>
-      {!user
-      ?
-    <Outlet/>
-    :
-    <Navigate to="/" replace/>
-  }
-
-    </Route>
-  )
-}
-
-
 function App() {
   
  
   return (
-   <>
- 
- <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        
-      </Routes>
-    </BrowserRouter>
-  
-  
+   <>  
    </>
   );
 }
