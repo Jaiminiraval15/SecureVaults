e2e17d9b31c45bf39c7f5e6f3d07dd624997d4fe
Jaimini <jaiminiraval123@gmail.com>
2024-02-05 20:13:31 +0530

    Add settings menu for account control

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,9 @@
 import User from './User';
+import {Container,Toolbar,Box,Menu,MenuItem,Typography,IconButton,Tooltip,Avatar} from '@mui/material';
 import { Navbar } from 'react-bootstrap';
+import AppBar from '@mui/material/AppBar';
 export default function Nav() {
-
+    const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
     return (
         <>
          <Navbar expand="lg" className="bg-body-tertiary d-flex align-items-center" style={{
@@ -15,6 +17,7 @@ export default function Nav() {
             }}>SecureVaults
        
             </Navbar>
+   
             <User/>
        
         </>
--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Button, Dialog, DialogContent, DialogTitle, IconButton } from '@mui/material';
+import { Button, Dialog, MenuItem, Avatar, Menu, DialogContent, DialogTitle, IconButton } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 import LoginForm from './Forms/LoginForm';
 import SignupForm from './Forms/SignupForm';
@@ -7,79 +7,72 @@ import { useState } from 'react';
 import { useLogout } from '../hooks/useLogout';
 import { useAuthContext } from '../hooks/useAuthContext';
 import { useNavigate } from 'react-router-dom';
-
+import AccountBoxIcon from '@mui/icons-material/AccountBox';
+import LogoutIcon from '@mui/icons-material/Logout';
 export default function User() {
   const [showForm, setShowForm] = useState(null);
   const { logout } = useLogout();
   const { user } = useAuthContext();
   const navigate = useNavigate();
-  const openSignup = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+
+    const openSignup = () => {
     setShowForm('signup');
   };
 
   const openLogin = () => {
     setShowForm('login');
-   
+  };
+  const openMenu = (event) => {
+    setAnchorEl(event.currentTarget);
   };
 
   const handleClose = () => {
-    setShowForm(null);
+  
+      setShowForm(null);
+    
+    setAnchorEl(null);
   };
 
-  const handleSubmit = () => {
+  const handleLogout = () => {
+    handleClose();
     logout();
-    navigate('/')
+    navigate('/');
   };
-  
 
   return (
     <>
       {user && (
         <>
-        <Link  style={{ textDecoration: 'none' }}>
-          <Button
-            variant="outlined"
-            color="primary"
-            style={{
-              color: 'purple',
-              padding: '0.5em',
-              display: 'block',
-              float: 'right',
-              marginRight: '1em',
-              marginTop: '0.5em'
+           
+          <Avatar src="/broken-image.jpg" style={{ float: 'right', marginRight: '1em', marginTop: '0.5em' }} onClick={openMenu}></Avatar>
+          <Menu
+            anchorEl={anchorEl}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+            getContentAnchorEl={null}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
             }}
-            onClick={handleSubmit}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
           >
-            Logout
-          </Button>
-        </Link>
-        <Link to="/vault"   style={{
-              color: 'purple',
-              padding: '0.5em',
-              display: 'block',
-              float: 'right',
-              marginRight: '1em',
-              marginTop: '0.5em'
-            }}>Vault</Link>
-             <Link to="/folder"   style={{
-              color: 'purple',
-              padding: '0.5em',
-              display: 'block',
-              float: 'right',
-              marginRight: '1em',
-              marginTop: '0.5em'
-            }}>Folder</Link>
-             <Link to="/generatePassword"   style={{
-              color: 'purple',
-              padding: '0.5em',
-              display: 'block',
-              float: 'right',
-              marginRight: '1em',
-              marginTop: '0.5em'
-            }}>Generate Password</Link>
+            <MenuItem onClick={handleClose}><AccountBoxIcon style={{marginInline:'0.2em'}}/>Profile</MenuItem>
+            <MenuItem onClick={handleLogout}><LogoutIcon style={{marginInline:'0.2em'}}/>Logout</MenuItem>
+          </Menu>
+        
+          <div>
+            <Link to="/vault" style={{ color: 'purple', padding: '0.5em', display: 'block', float: 'right', marginRight: '1em', marginTop: '0.5em' }}>Vault</Link>
+            <Link to="/folder" style={{ color: 'purple', padding: '0.5em', display: 'block', float: 'right', marginRight: '1em', marginTop: '0.5em' }}>Folder</Link>
+            <Link to="/generatePassword" style={{ color: 'purple', padding: '0.5em', display: 'block', float: 'right', marginRight: '1em', marginTop: '0.5em' }}>Generate Password</Link>
+          </div>
         </>
       )}
-      {!user && (
+
+{!user && (
         <>
           <Button
             variant="outlined"
@@ -139,8 +132,8 @@ export default function User() {
             </DialogContent>
           </Dialog>
         
-        </>
-      )}
     </>
+)}
+</>
   );
 }
